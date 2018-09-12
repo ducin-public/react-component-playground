9b943c3ec72a9e9102e0ccc5599c2358b867b54e
Tomasz Ducin <tomasz.ducin@gmail.com>
2018-09-12 22:23:13 +0200

    minor styles change

--- src/App.js
+++ src/App.js
@@ -7,10 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <h1 className="App-title">Component Playground Demo</h1>
         <p className="App-intro">
           To get started, edit <code>src/examples/component.example.js</code> and save to reload.
         </p>
